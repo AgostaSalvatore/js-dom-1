928fd20f07ddcb183cdc334c8ea3f0d440c25021
AgostaSalvatore <agosta.boolean@gmail.com>
2025-03-25 13:31:46 +0100

    created variables for lamp

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// Creare un interruttore che al click cambia lo stato della lampadina da accesa a spenta e viceverssa.
+// Il cambio di stato si riflette anche sull'immagine della lampadina.
+
+// Variabili
+const lampadina = document.getElementById('lampadina');
+const interruttore = document.getElementById('btn-on-off');
+
+
+  
