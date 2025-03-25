e3b77a9e5fd563430f3b2758f987a9e859bdd9dd
AgostaSalvatore <agosta.boolean@gmail.com>
2025-03-25 13:35:10 +0100

    Toggle lampadina image source on button click

--- js/script.js
+++ js/script.js
@@ -6,6 +6,11 @@ const interruttore = document.getElementById('btn-on-off');
 //Inizializzazione eventi
 //Accensione e spegnimento lampadina tramite bottone
 interruttore.addEventListener('click', function() {
- 
+    if (lampadina.src.includes('yellow')) {
+        lampadina.src = './img/white_lamp.png';
+
+    } else {
+        lampadina.src = './img/yellow_lamp.png';
+    }
 });
   
